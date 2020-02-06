eb79799ae7d3672889c07905b5d66635e1470356
czachariah <zachariahchris@yahoo.com>
2020-02-06 17:41:01 -0500

    started work on readig commands

--- ProjectManager.java
+++ ProjectManager.java
@@ -1,7 +1,6 @@
 import java.util.Scanner; 
 /**
-  
- @author  
+ @author Chris Zachariah (cvz2)
  */
  
 public class ProjectManager
@@ -10,36 +9,55 @@ public class ProjectManager
    Team cs213;
    public void run()
    {
-       
-      boolean done = false;
-      while ( !done )
-      {
+	   stdin = new Scanner(System.in);
+	   boolean done = false;
+	   while ( !done )
+	   {
          String command = stdin.next();
-         switch (command)  
-         {   
-            case "A": add();
-		      break;     
-            default: //deal with bad command here 
-         }  
-      }
+        
+         if (command.length() != 1) // make sure that the command is formatted correctly
+         {
+        	 if (command.charAt(1) != ' ')
+        	 {
+        		 
+        	 }
+        	 System.out.println("here");
+         }
+         switch (command.charAt(0))  
+         {
+            case 'A': add();
+		    	break;
+            case 'R': remove();
+            	break;
+            case 'P': print();
+            	break;
+            case 'Q': print(); done = !done;
+            	break;
+            default: System.out.println("Command '"+ command.charAt(0) +"' is not supported");	//deal with bad command here 
+         } // ends the switch statement 
+      } // ends the while loop
       //write java code before you terminate the program
-   } //run()
+      System.out.println("The team is ready to go!");
+   } // run()
    
    private void add()
    {
+	   System.out.println("adding");
       	//must check if the date is valid
 	//must call the contains() method to check if a given 
 	//team member is in the team already
-   }
+   } // add()
    
    private void remove()
    {
+	   System.out.println("removing");
       //must check if the date is valid
 	   
-   }
+   } // remove()
    
    private void print()
    {
+	   System.out.println("printing");
       //must check if the team has 0 members. 
-   }   
+   } // print()
 } //ProjectManager
